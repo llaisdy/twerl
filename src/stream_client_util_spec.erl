eae4d86c897d908a73052a1d046ab07e69d09197
Luca Spiller <luca@stackednotion.com>
2011-11-11 20:20:49 +0000

    Wrap specs in describe block for module name

--- src/stream_client_util_spec.erl
+++ src/stream_client_util_spec.erl
@@ -9,57 +9,59 @@ test() ->
 
 spec() ->
     [
-        ?_describe("#generate_headers", [
-                ?_it("should generate Host and User-Agent headers", fun() ->
-                            Result = stream_client_util:generate_headers(),
-                            Expected = [
-                                {"Host", "api.twitter.com"},
-                                {"User-Agent", "Twerl"}
-                            ],
+        ?_describe("stream client util", [
+                ?_describe("#generate_headers", [
+                        ?_it("should generate Host and User-Agent headers", fun() ->
+                                    Result = stream_client_util:generate_headers(),
+                                    Expected = [
+                                        {"Host", "api.twitter.com"},
+                                        {"User-Agent", "Twerl"}
+                                    ],
 
-                            ?assertEqual(Expected, Result)
-                    end)
-            ]),
+                                    ?assertEqual(Expected, Result)
+                            end)
+                    ]),
 
-        ?_describe("#generate_auth_headers", [
-                ?_it("should include default headers if none passed", fun() ->
-                            Result = stream_client_util:generate_auth_headers("user", "pass"),
-                            Headers = stream_client_util:generate_headers(),
-                            Expected = [
-                                {"Authorization", "Basic " ++ binary_to_list(base64:encode("user" ++ ":" ++ "pass"))} | Headers
-                            ],
+                ?_describe("#generate_auth_headers", [
+                        ?_it("should include default headers if none passed", fun() ->
+                                    Result = stream_client_util:generate_auth_headers("user", "pass"),
+                                    Headers = stream_client_util:generate_headers(),
+                                    Expected = [
+                                        {"Authorization", "Basic " ++ binary_to_list(base64:encode("user" ++ ":" ++ "pass"))} | Headers
+                                    ],
 
-                            ?assertEqual(Expected, Result)
-                    end),
+                                    ?assertEqual(Expected, Result)
+                            end),
 
-                ?_it("should allow custom headers to be passed", fun() ->
-                            Result = stream_client_util:generate_auth_headers("user", "pass", []),
-                            Expected = [
-                                {"Authorization", "Basic " ++ binary_to_list(base64:encode("user" ++ ":" ++ "pass"))}
-                            ],
+                        ?_it("should allow custom headers to be passed", fun() ->
+                                    Result = stream_client_util:generate_auth_headers("user", "pass", []),
+                                    Expected = [
+                                        {"Authorization", "Basic " ++ binary_to_list(base64:encode("user" ++ ":" ++ "pass"))}
+                                    ],
 
-                            ?assertEqual(Expected, Result)
-                    end)
-            ]),
+                                    ?assertEqual(Expected, Result)
+                            end)
+                    ]),
 
-        ?_describe("#userids_to_follow", [
-                ?_it("should return an error when no users are passed", fun() ->
-                            Result = stream_client_util:userids_to_follow([]),
-                            Expected = {error, no_args_passed},
+                ?_describe("#userids_to_follow", [
+                        ?_it("should return an error when no users are passed", fun() ->
+                                    Result = stream_client_util:userids_to_follow([]),
+                                    Expected = {error, no_args_passed},
 
-                            ?assertEqual(Expected, Result)
-                    end),
+                                    ?assertEqual(Expected, Result)
+                            end),
 
-                ?_it("should return the correct url for one user", fun() ->
-                            Result = stream_client_util:userids_to_follow(["1"]),
-                            Expected = {ok, "follow=1"},
-                            ?assertEqual(Expected, Result)
-                    end),
+                        ?_it("should return the correct url for one user", fun() ->
+                                    Result = stream_client_util:userids_to_follow(["1"]),
+                                    Expected = {ok, "follow=1"},
+                                    ?assertEqual(Expected, Result)
+                            end),
 
-                ?_it("should return the correct url for two users", fun() ->
-                            Result = stream_client_util:userids_to_follow(["1", "2"]),
-                            Expected = {ok, "follow=1,2"},
-                            ?assertEqual(Expected, Result)
-                    end)
+                        ?_it("should return the correct url for two users", fun() ->
+                                    Result = stream_client_util:userids_to_follow(["1", "2"]),
+                                    Expected = {ok, "follow=1,2"},
+                                    ?assertEqual(Expected, Result)
+                            end)
+                    ])
             ])
     ].
